5c685fd5ddc2915b6ae04623f75c3435ecd83641
Joseph Jones <jjones15470@hotmail.com>
2017-05-08 08:14:36 -0600

    Created the base projects in order to seperate server and client

---
+++ Server/Server.cs
@@ -0,0 +1,14 @@
+﻿using ChatBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server {
+    class Server : IServer {
+        static void Main(string[] args) {
+
+        }
+    }
+}
