9b99f2e8fea8e821553db1175cc19223fd8eda56
Joseph Jones <jjones15470@hotmail.com>
2017-05-08 09:35:38 -0600

    Changed server to use a GUI

--- ChatBase/ChatBase.cs
+++ ChatBase/ChatBase.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// WebClient / WebRequest
+// System.Net.Sockets
+
 namespace ChatBase {
     public interface IServer {
 
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,24 +1,12 @@
-﻿using ChatBase;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChatBase;
 
 namespace Server {
     public class Server : IServer {
-        
-    }
 
-    public class Startup {
-        public static void Start() {
-            Server server = new Server();
-
-
-        }
-
-        public static void Main() {
-            Start();
-        }
     }
 }
