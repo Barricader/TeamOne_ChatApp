99f3847d3aaf577c14981acfbac56896671c7620
jaguilar12 <jaguilar@student.neumont.edu>
2017-05-10 18:01:49 -0600

    Another test comment

--- ChatBase/ChatBase.cs
+++ ChatBase/ChatBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 // WebClient / WebRequest
 // System.Net.Sockets
-//This is my test comment
+//This is my test comment - Juan Aguilar
 
 namespace ChatBase {
     public interface IServer {
