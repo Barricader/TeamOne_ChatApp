6425dd8d051951807867867838f556c65088c1ee
smithbinah <bsmith@student.neumont.edu>
2017-05-17 18:41:01 -0600

    Added converters for hint text

---
+++ LoginWindow/Converters/BooltoVisConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LoginWindow.Converters
+{
+    class BooltoVisConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = (bool) value;
+            
+
+            return (flag ? Visibility.Visible : Visibility.Hidden);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
