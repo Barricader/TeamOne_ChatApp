9a3f3f587957e12e11311c84224f5cd562fc3540
DarkFantom10 <sstein13@gmail.com>
2017-05-17 18:13:40 -0600

    Added MySqlConnectors to DBConnect class, successfully added tables and databases through code

--- Database/DbConnect.cs
+++ Database/DbConnect.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql.Data.MySqlClient;
 namespace Database
 {
-    class Program
+    class DbConnect
     {
         static void Main(string[] args)
         {
+            string connectStr = "server=localhost;user=root;database=test;port=3306;password=DarkFantom10;";
+            MySqlConnection conn = new MySqlConnection(connectStr);
+            try
+            {
+                Console.WriteLine("Conneting to database...");
+                conn.Open();
+                string createTable = "create table testCode (swiggity int(15), name char(14))";
+                MySqlCommand cmd = new MySqlCommand(createTable, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + "---" + rdr[1]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
