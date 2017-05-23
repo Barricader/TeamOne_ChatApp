84c983a72170bee277292d5b43e9580b9d615a30
Joseph Jones <jjones15470@hotmail.com>
2017-05-23 17:32:09 -0600

    Added Packet class to handle communication between client and server

---
+++ ChatBase/Models/Packet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBase.Models {
+    public class Packet {
+        public string Type { get; set; }
+        public string Content { get; set; }
+        public Dictionary<string, string> Args { get; set; }
+
+        public Packet(string type, string content) {
+            Type = type;
+            Content = content;
+            Args = new Dictionary<string, string>();
+        }
+
+        public Packet(string type, string content, Dictionary<string, string> args) {
+            Type = type;
+            Content = content;
+            Args = args;
+        }
+    }
+}
