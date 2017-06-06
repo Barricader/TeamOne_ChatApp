b6ba993430296efa5eef2fb6e6786b86da2c38c1
Joseph Jones <jjones15470@hotmail.com>
2017-06-06 17:59:18 -0600

    Finished metadata

--- MainClientWindow/MainWindow.xaml.cs
+++ MainClientWindow/MainWindow.xaml.cs
@@ -14,10 +14,9 @@ namespace MainClientWindow {
 
           
             Client client = (Client)FindResource("client");
-            client.Start();
+            //client.Start();
 
-            WindowsNotification wn = new WindowsNotification();
-            //mainframe.NavigationService.Navigate(new Uri("ChatPage.xaml", UriKind.RelativeOrAbsolute));
+            mainframe.NavigationService.Navigate(new Uri("ChatPage.xaml", UriKind.RelativeOrAbsolute));
 
             System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon() {
                 Icon = Properties.Resources.iconthing,
